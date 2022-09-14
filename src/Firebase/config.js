e445569574199a7bc5b922760790723329549d2d
Muhammed Ajeer <muhdajeer@gmail.com>
2022-09-14 10:43:43 +0530

    completed

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -6,13 +6,13 @@ import 'firebase/firebase-firestore'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey:"API_key",
-    authDomain: "Auth domain",
-    projectId: "project id",
-    storageBucket: "storage bucket",
-    messagingSenderId: "mesaging id",
-    appId: "appid",
-    measurementId: "measurmentid"
-  };
+  apiKey: "AIzaSyCDdLWje-N3PsdgNTjs1kapdv246MhbgN0",
+  authDomain: "olx-clone-f50b4.firebaseapp.com",
+  projectId: "olx-clone-f50b4",
+  storageBucket: "olx-clone-f50b4.appspot.com",
+  messagingSenderId: "722509281686",
+  appId: "1:722509281686:web:b75e10f4fdb770b1a7c5b7",
+  measurementId: "G-S4YT9F7K3Y"
+};
 
  export default firebase.initializeApp(firebaseConfig)
